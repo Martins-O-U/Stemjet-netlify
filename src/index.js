5adb438aa43e7cb3ff2db124c1969e3cac4f3fc1
HP <martinsonyedikachi@gmail.com>
2020-06-17 17:57:48 +0100

    Deleted state management

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-import { AllBlogPost, SingleBlogPost } from './State-management/Reducers/reducer';
 
-const monsterReducer = combineReducers({
-    BlogPosts: AllBlogPost,
-    SingleBlogPost: SingleBlogPost,
-});
-
-const store = createStore(
-    monsterReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
-
-ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
